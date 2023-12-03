db6f776592c7ce6984b15e5ce7ab6d234e11652b
jkhuysmans <149975556+jkhuysmans@users.noreply.github.com>
2023-12-04 00:20:18 +0100

    set user prompt to change squares side

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,19 +1,38 @@
 let container = document.querySelector(".squareContainer");
+let squareSide = 16;
 
-for (let i = 0; i < (16 * 16); i++) {
+let squarePrompt = prompt("Insert the number of squares per side you want");
+    squareSide = parseInt(squarePrompt);
+    
+addSquares();
+
+function addSquares() { for (let i = 0; i < (squareSide * squareSide); i++) {
+    
     let squareDiv = document.createElement("div");
     squareDiv.classList.add("square");
+    squareDiv.style.width = `${512 / squareSide}px`;
+    squareDiv.style.height = `${512 / squareSide}px`;
 
     squareDiv.addEventListener('mouseenter', () => {
         squareDiv.style.backgroundColor = "grey";
     });
 
     container.appendChild(squareDiv);
-}
-
+}};
 
 let track = document.querySelector('.square');
 
-track.addEventListener('mouseenter', () => {
-    track.style.backgroundColor = "red";
-});
+let changeButton = document.querySelector(".changebutton")
+changeButton.addEventListener("click", changeSquares)
+
+function changeSquares() {
+    container.innerHTML = "";
+    
+    let squarePrompt = prompt("Insert the number of squares per side you want");
+    squareSide = parseInt(squarePrompt);
+    console.log(squarePrompt)
+
+
+    
+    addSquares(squareSide);
+}
