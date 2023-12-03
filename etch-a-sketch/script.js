d6bf142fb5fede7111d828dd40c681b7b1444575
jkhuysmans <149975556+jkhuysmans@users.noreply.github.com>
2023-12-03 19:38:20 +0100

    fix divs and add grey hoverign

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,9 +1,19 @@
 let container = document.querySelector(".squareContainer");
 
-
-for (let i = 1; i < 16 * 16; i++) {
+for (let i = 0; i < (16 * 16); i++) {
     let squareDiv = document.createElement("div");
     squareDiv.classList.add("square");
 
-    container.appendChild(squareDiv)
-} 
+    squareDiv.addEventListener('mouseenter', () => {
+        squareDiv.style.backgroundColor = "grey";
+    });
+
+    container.appendChild(squareDiv);
+}
+
+
+let track = document.querySelector('.square');
+
+track.addEventListener('mouseenter', () => {
+    track.style.backgroundColor = "red";
+});
